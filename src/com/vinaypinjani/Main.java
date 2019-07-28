aff2d47f6abda0e7de3193916b5683eb8d38ddc4
vinaypinjani <35225431+vinaypinjani@users.noreply.github.com>
2019-07-28 13:57:02 +0500

    Initial commit

---
+++ src/com/vinaypinjani/Main.java
@@ -0,0 +1,21 @@
+package com.vinaypinjani;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay >= 0 && hourOfDay <= 23) {
+            if (barking && (hourOfDay < 8 || hourOfDay > 22)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
